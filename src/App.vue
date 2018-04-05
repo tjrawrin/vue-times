121658c71467f2c2f8dacd2ed0f3c855372f29af
Timothy Warrington <tim.warrin@gmail.com>
2018-04-05 15:33:40 -0600

    Remove boilerplate content and add some default styles

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default Vue.extend({
   name: 'app',
-  components: {
-    HelloWorld,
-  },
 });
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+html {
+  box-sizing: border-box;
+  font-size: 62.5%;
+}
+body {
+  background: #00c9ff;
+  background: -webkit-linear-gradient(to bottom, #92fe9d, #00c9ff);
+  background: linear-gradient(to bottom, #92fe9d, #00c9ff);
+  color: #333333;
+  font-family: 'Varela Round', sans-serif;
+  font-size: 1.6rem;
+  height: 100vh;
+  line-height: 1.75;
+  margin: 0;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
